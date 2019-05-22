228a0db48318237f20ae317baae9473836be4491
arqpml <arq.marinho@gmail.com>
2019-05-22 14:54:43 -0300

    visual studio

---
+++ Class1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Autodesk.Navisworks.Api;
+using Autodesk.Navisworks.Api.Plugins;
+
+namespace NavisCheckerSener
+{
+    [PluginAttribute("Sener Element Checker",
+          //Plugin name
+          "ADSK",
+                //character Developer ID or GUID
+                ToolTip = "Select Element to Display Information",
+                //The tooltip for the item in the ribbon
+                DisplayName = "Sener Element Checker")]
+    public class NavisModelCheker : AddInPlugin
+    {
+        
+        public override int Execute(params string[] parameters)
+        {
+            MessageBox.Show("Teste");
+            return 0;
+        }
+    }
+}
